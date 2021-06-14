4999b2b4fc45d123cf8ae860cd947254c0310766
Steven Abaco <stevenabaco@gmail.com>
2021-06-13 21:36:37 -0500

    Add Courses component, fix Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React, {useEffect} from 'react';
-import { BrowserRouter as Router, Route, Switch, useHistory, Link } from 'react-router-dom';
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+} from 'react-router-dom';
 
 import Header from './Components/Header';
 import Courses from './Components/Courses';
 
 function App() {
-  useEffect(() => {
-		fetch('http://localhost:5000/api/courses')
-	}, []);
 	return (
 		<Router>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Courses} />
-      </Switch>
-      
+				<Header />
+			<Switch>
+				<Route exact path='/' component={Courses} />
+			</Switch>
 		</Router>
 	);
 }
--- client/src/Components/Courses.js
+++ client/src/Components/Courses.js
@@ -1,42 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Courses = () => {
-  const [courses, setCourses] = useState([]);
+	const [courses, setCourses] = useState([]);
+	let history = useHistory();
 
-  return (
+	useEffect(() => {
+		fetch('http://localhost:5000/api/courses')
+			.then(res => {
+				if (res.status === 200) {
+					return res.json().then(res => setCourses(res));
+				} else if (res.status === 500) {
+					history.push('/error');
+				}
+			})
+			.catch(err => {
+				console.log(err);
+				history.push('/error');
+			});
+	}, [history]);
+	console.log(courses);
+	return (
 		<main>
-			<div class='wrap main--grid'>
-				<a class='course--module course--link' href='course-detail.html'>
-					<h2 class='course--label'>Course</h2>
-					<h3 class='course--title'>Build a Basic Bookcase</h3>
-				</a>
-				<a class='course--module course--link' href='course-detail.html'>
-					<h2 class='course--label'>Course</h2>
-					<h3 class='course--title'>Learn How to Program</h3>
-				</a>
-				<a class='course--module course--link' href='course-detail.html'>
-					<h2 class='course--label'>Course</h2>
-					<h3 class='course--title'>Learn How to Test Programs</h3>
-				</a>
-				<a class='course--module course--add--module' href='create-course.html'>
-					<span class='course--add--title'>
+			<div className='wrap main--grid'>
+				{courses.map(course => {
+					return (
+						<Link
+							key={course.id}
+							className='course--module course--link'
+							to={`courses/$(course.id)`}>
+							<h2 className='course--label'>Course</h2>
+							<h3 className='course--title'>{course.title}</h3>
+						</Link>
+					);
+				})}
+
+				<Link
+					className='course--module course--add--module'
+					to='/courses/create'>
+					<span className='course--add--title'>
 						<svg
 							version='1.1'
 							xmlns='http://www.w3.org/2000/svg'
 							x='0px'
 							y='0px'
 							viewBox='0 0 13 13'
-							class='add'>
+							className='add'>
 							<polygon points='7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 '></polygon>
 						</svg>
 						New Course
 					</span>
-				</a>
+				</Link>
 			</div>
 		</main>
 	);
-}
+};
 
 export default Courses;
